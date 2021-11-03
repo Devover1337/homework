f2189431dadf77fee203da8368856736672173d6
Devover <serzh.onyushkin@mail.ru>
2021-11-03 23:26:42 +0300

    add file lesson1_task1
    completing a task

--- lesson1_task1.py
+++ lesson1_task1.py
@@ -1,1 +1,17 @@
-print('hello world')
+#print('hello world')
+# Залача номер 2 из первого урока по Python
+def sum_digits(value):
+    result = 0
+
+    while value != 0:
+        result += value % 10
+        value //= 10
+    return result
+
+arr = [i ** 3 for i in range(1, 1001, 2)]
+
+result1 = sum(filter(lambda num: sum_digits(num) % 7 == 0, arr))
+result2 = sum(filter(lambda num: sum_digits(num + 17) % 7 == 0, arr))
+
+print(result1)
+print(result2)
