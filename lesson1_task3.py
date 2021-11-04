a554dbe748c38cb2324e85309786bd0445a161cc
Devover <serzh.onyushkin@mail.ru>
2021-11-04 22:23:51 +0300

    add file lesson1_task1
    completing a task
    
    add file lesson1_task2(completed)
    
    add file lesson1_task3(completed)

---
+++ lesson1_task3.py
@@ -0,0 +1,34 @@
+# Задача номер 3 первого урока по изучению Python
+
+numb = int(input('Введите тестируемое число: '))
+
+
+
+if numb >= 11 or numb <= 19:
+    print(numb, 'процентов')
+
+elif numb % 10 == 1:
+    print(numb, 'процент')
+
+
+elif numb % 10 == 5:
+    print(numb, 'процентов')
+
+elif numb % 10 == 6:
+    print(numb, 'процентов')
+
+elif numb % 10 == 7:
+    print(numb, 'процентов')
+
+elif numb % 10 == 8:
+    print(numb, 'процентов')
+
+elif numb % 10 == 9:
+    print(numb, 'процентов')
+
+elif numb % 10 == 0:
+    print(numb, 'процентов')
+
+
+else:
+    print(numb, 'процента')
