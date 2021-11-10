ec143e81b26668ee62223294d9c5c55a0d1b0e53
Devover <serzh.onyushkin@mail.ru>
2021-11-10 13:47:18 +0300

    add file lesson2_task1(completed)

---
+++ lesson2_task1.py
@@ -0,0 +1,17 @@
+# Задача номер 1 второго урока по Python
+
+number1 = 15 * 3
+print(number1)
+print(type(number1))
+
+number2 = 15 / 3
+print(number2)
+print(type(number2))
+
+number3 = 15 // 2
+print(number3)
+print(type(number3))
+
+number4 = 15 ** 2
+print(number4)
+print(type(number4))
