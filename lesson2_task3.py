c7bd6882005b0b00f7a54321efd209c8ca2701e9
Devover <serzh.onyushkin@mail.ru>
2021-11-10 16:17:35 +0300

    add file lesson2_task3(completed)

---
+++ lesson2_task3.py
@@ -0,0 +1,21 @@
+main = ['инженер-конструктор Игорь', 'главный бухгалтер МАРИНА', 'токарь высшего разряда нИКОЛАй', 'директор аэлита']
+
+list_1 = main[0]
+list_main = list_1.split(' ')
+name1 = list_main[-1]
+print('Привет, ', name1.capitalize())
+
+list_2 = main[1]
+list_main = list_2.split(' ')
+name2 = list_main[-1]
+print('Привет, ', name2.capitalize())
+
+list_3 = main[2]
+list_main = list_3.split(' ')
+name3 = list_main[-1]
+print('Привет, ', name3.capitalize())
+
+list_4 = main[3]
+list_main = list_4.split(' ')
+name4 = list_main[-1]
+print('Привет, ', name4.capitalize())
